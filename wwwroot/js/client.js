128d9a45906daf5ad8636f1b10e75a0700a6817f
Eric Liu <eliu@my.wctc.edu>
2019-09-10 20:51:54 -0500

    module2hw

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,16 @@
 ﻿$(function(){
     //alert("Hello World");
+    //$('.toast').toast('show');
+
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+
+    $('.form-check-input').on('change', function () {
+        $('#' + this.id + 'Img').css('visibility', 'visible');
+
+        $(this).is(':checked') ?
+            $('#' + this.id + 'Img').removeClass().addClass('animated slideInUp') :
+            $('#' + this.id + 'Img').addClass('animated slideOutDown');
+    });
 });
